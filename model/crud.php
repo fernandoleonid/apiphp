644c47ed3110b8846ed1e6319b01cae7fc9f83de
fernandoleonid <fernandoleonid@gmail.com>
2019-11-21 14:01:20 -0300

    Prevenindo contra SQL injection

--- model/crud.php
+++ model/crud.php
@@ -3,44 +3,55 @@
     function create ($resource, $data) {
         $conn =  include_once("./model/connnection.php");
 
-        function quotes ( $value ) {
-            return "'$value'";
-        }
-        $campos = implode (",", array_keys($data));
-        $valores = implode (",", array_map("quotes", $data));
+        $fields = implode (",", array_keys($data));
+        $records = array_values($data);
+        $values =  implode(',', array_fill(0, count($data), '?'));        
+        $sql = "INSERT INTO $resource ( $fields ) VALUES ( $values )";
+        $types = str_repeat('s', count($data));
 
-        $sql = "INSERT INTO $resource ($campos) VALUES ($valores)";
-        $result = $conn->query($sql);
-        return $result;
+        $cmd = $conn->prepare($sql);
+
+        $cmd->bind_param($types,...$records);
+
+        $result = $cmd->execute();
+        return $result ;
+
     }
 
     function read ($resource, $id=null) {
         $conn =  include_once("./model/connnection.php");
-        $sql = $id ? "SELECT * FROM $resource WHERE id=$id" : $sql = "SELECT * FROM $resource";
-        $result = $conn->query($sql);
-        return $result ? $result->fetch_all(MYSQLI_ASSOC) : $result;
-
+        $sql = "SELECT * FROM $resource WHERE ?='' or id=?";
+        $cmd = $conn->prepare($sql);
+        $cmd->bind_param('ss',$id,$id);
+        $result = $cmd->execute();
+        return $result ? $cmd->get_result()->fetch_all(MYSQLI_ASSOC) : $result;
     }
     
     function update ($resource, $id, $data) {
         $conn =  include_once("./model/connnection.php");
 
-        $update = "";
-        foreach ( $data as $field => $value ){
-            $update .= "$field='$value',";
-        }
-        $update = trim($update,",");
-
+        $update = implode ("=?,", array_keys($data)) . "=?";
+        $records = array_values($data);
+        $types = str_repeat('s', count($data));
         $sql = "UPDATE $resource SET $update WHERE id=$id";
-        $result = $conn->query($sql);
-        return  $result;
+
+        $cmd = $conn->prepare($sql);
+        $cmd->bind_param($types,...$records);
+
+        $result = $cmd->execute();
+        return $result ;
         
     }
 
     function delete ( $resource, $id ) {
         $conn = include_once ("./model/connnection.php");
-        $sql = "DELETE FROM $resource WHERE ID=$id";
-        return $conn->query($sql);
+        $sql = "DELETE FROM $resource WHERE id=?";
+        
+        $cmd = $conn->prepare($sql);
+        $cmd->bind_param('s',$id);
+        $result = $cmd->execute();
+
+        return $result;
         
     }
 
